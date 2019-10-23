49754326b75d7395c8e22797382b24c9100bbd2b
Filip106 <djordjevic.filip994@gmail.com>
2019-10-24 01:49:12 +0200

    change indent

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from '../../assets/images/logo.svg';
 import {BrowserRouter as Router} from "react-router-dom";
 import './App.css';
 import NavBar from './NavBar.js';
@@ -7,21 +6,22 @@ import Nav from "../Nav/Nav";
 import Pages from "../Pages/Pages";
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="App">
-              <NavBar/>
-              <div>
-                <Router>
-                  <div>
-                    <Nav/>
+  render() {
+    return (
+      <div className="App">
+        <NavBar/>
+        <div>
+          <Router>
+            <div>
+              <Nav/>
 
-                    <Pages/>
-                  </div>
-                </Router>
-              </div>
+              <Pages/>
             </div>
-  );
+          </Router>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
